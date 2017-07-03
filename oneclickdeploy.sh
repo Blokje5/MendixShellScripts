0d32c7979f6facaffcd4d8e726f0d401d242b99e
Quion Testautomation <testautomation@quion.com>
2017-07-03 13:01:00 +0200

    Fix voor JQ variabele die niet goed wordt opgepakt.

--- oneclickdeploy.sh
+++ oneclickdeploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Mendix one click deploy
+JQ="D:\Devtools\JQ.exe"
 
 #Check flags and initialize parameters needed for REST calls
 while getopts ":a:b:u:n:m:" opt; do
@@ -30,16 +31,12 @@ RESPONSE=$(curl -s -X GET -H "Mendix-Username: $USER" -H "Mendix-ApiKey: $API_KE
 check_for_error()
 {
   #If it is not an array, we need to check if we have an error message
-  if [ "$RESPONSE" = "" ]
-  then
-    return
-  fi
-  ISARRAY=$(echo $RESPONSE | jq 'if type=="array" then 1 else 0 end')
+  ISARRAY=$(echo $RESPONSE | $JQ 'if type=="array" then 1 else 0 end')
   if (( $ISARRAY < 1 ))
   then
-    if echo $RESPONSE | jq -e 'has("errorMessage")' > /dev/null
+    if echo $RESPONSE | $JQ -e 'has("errorMessage")' > /dev/null
     then
-      ERROR=$(echo $RESPONSE | jq -r ".errorMessage")
+      ERROR=$(echo $RESPONSE | $JQ -r ".errorMessage")
       echo "Error response from server:"
       echo "$ERROR"
       exit 1
@@ -47,7 +44,7 @@ check_for_error()
   fi
 }
 check_for_error
-NUMBER=$(echo $RESPONSE | jq ".[0] |  .Number ")
+NUMBER=$(echo $RESPONSE | $JQ ".[0] |  .Number ")
 #Generate body for package request
 generate_body_package()
 {
@@ -63,13 +60,13 @@ EOF
 #Build package
 RESPONSE=$(curl -s -X POST -H "Mendix-Username: $USER" -H "Content-Type: application/json" -H "Mendix-ApiKey: $API_KEY" -d "$(generate_body_package)" https://deploy.mendix.com/api/1/apps/$APP_ID/packages/)
 check_for_error
-PACKAGEID=$(echo $RESPONSE | jq -r '.PackageId')
+PACKAGEID=$(echo $RESPONSE | $JQ -r '.PackageId')
 #check if status is Succeeded, else try again
 check_package_status()
 {
     local RESPONSE=$(curl -s -X GET -H "Mendix-Username: $USER" -H "Content-Type: application/json" -H "Mendix-ApiKey: $API_KEY" https://deploy.mendix.com/api/1/apps/$APP_ID/packages/$PACKAGEID/ )
     check_for_error
-    local STATUS=$(echo $RESPONSE | jq -r '.Status')
+    local STATUS=$(echo $RESPONSE | $JQ -r '.Status')
     local CODE="Succeeded"
     if [ "$STATUS" == "$CODE" ]
     then 
@@ -89,7 +86,7 @@ check_environment_status()
 {
   local RESPONSE=$(curl -s -X GET -H "Mendix-Username: $USER" -H "Mendix-ApiKey: $API_KEY" "https://deploy.mendix.com/api/1/apps/$APP_ID/environments/$MODE/")
   check_for_error
-  local STATUS=$(echo $RESPONSE | jq -r '.Status')
+  local STATUS=$(echo $RESPONSE | $JQ -r '.Status')
   local CODE="Stopped"
   if [ "$STATUS" == "$CODE" ]
   then
@@ -117,7 +114,7 @@ check_packageid_on_environment()
 {
   local RESPONSE=$(curl -s -X GET -H "Mendix-Username: $USER" -H "Mendix-ApiKey: $API_KEY" "https://deploy.mendix.com/api/1/apps/$APP_ID/environments/$MODE/package/")
   check_for_error
-  local STATUS=$(echo $RESPONSE | jq -r '.PackageId')
+  local STATUS=$(echo $RESPONSE | $JQ -r '.PackageId')
   if [ "$STATUS" == "$PACKAGEID" ]
   then
     echo "Package transported to environment"
@@ -138,13 +135,13 @@ generate_body_start()
 EOF
 }
 RESPONSE=$(curl -s -X POST -H "Mendix-Username: $USER" -H "Content-Type: application/json" -H "Mendix-ApiKey: $API_KEY"  -d "$(generate_body_start)" "https://deploy.mendix.com/api/1/apps/$APP_ID/environments/$MODE/start/")
-JOBID=$(echo $RESPONSE | jq -r '.JobId')
+JOBID=$(echo $RESPONSE | $JQ -r '.JobId')
 #check status Job
 check_job()
 {
   local RESPONSE=$(curl -s -X GET -H "Mendix-Username: $USER" -H "Mendix-ApiKey: $API_KEY" "https://deploy.mendix.com/api/1/apps/$APP_ID/environments/$MODE/start/$JOBID/")
   check_for_error
-  local STATUS=$(echo $RESPONSE | jq -r '.Status')
+  local STATUS=$(echo $RESPONSE | $JQ -r '.Status')
   local CODE="Started"
   if [ "$STATUS" == "$CODE" ]
   then
