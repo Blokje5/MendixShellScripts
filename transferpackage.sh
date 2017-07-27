471671792c98d51f5b6de5ed99b18646fd083ada
Lennard Eijsackers <lennard.eijsackers@finaps.nl>
2017-07-27 10:27:53 +0200

    More echoes

--- transferpackage.sh
+++ transferpackage.sh
@@ -66,9 +66,11 @@ generate_body_package()
 EOF
 }
 #Build package
-RESPONSE=$(curl -s -X POST -H "Mendix-Username: $USER" -H "Content-Type: application/json" -H "Mendix-ApiKey: $API_KEY" -d "$(generate_body_package)" https://deploy.mendix.com/api/1/apps/$APP_ID/packages/)
+RESPONSE=$(curl -s -v POST -H "Mendix-Username: $USER" -H "Content-Type: application/json" -H "Mendix-ApiKey: $API_KEY" -d "$(generate_body_package)" https://deploy.mendix.com/api/1/apps/$APP_ID/packages/)
+echo $RESPONSE
 check_for_error
 PACKAGEID=$(echo $RESPONSE | $JQ -r '.PackageId')
+echo $PACKAGEID
 #check if status is Succeeded, else try again
 check_package_status()
 {
